6f6b038e0f4aa8ba301d137f342d562b007c3496
Bartosz Spychalski <puntlik@outlook.com>
2019-06-12 12:07:31 +0200

    Publication center/editor stage completion
    Add-new MainActivity tab that redirects to publication center
    Add-feature for displaying all routes created by user in a form of a list
    Add-dedicated activity for viewing/editing user route
    Add-feature for displaying all data about picked user route
    Add-feature for setting bike type required/recommended for ceratin route
    Add-feature for setting route difficulty
    Add-feature for setting route description
    Add-feature for showing/hiding map in order to edit route in better
    comfort
    Add-feature for publishing user routes
    Modify-Route and RouteStop models in order to include newly added fields
    (description, difficulty etc.)
    Modify-new approach for displaying pictures both in publication editor
    and route tracker (less memory and CPU usage) - only one picture at a
    time is being shown
    Add-necessary strings to resources
    Modify-small refactor and code clean-up

---
+++ app/src/main/java/com/puntl/ibiker/companions/Base64Xcoder.kt
@@ -0,0 +1,20 @@
+package com.puntl.ibiker.companions
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Base64
+import java.io.File
+
+class Base64Encoder {
+    companion object {
+        fun imagePathToString(filePath: String): String {
+            val bytes = File(filePath).readBytes()
+            return Base64.encodeToString(bytes, Base64.DEFAULT)
+        }
+
+        fun stringToImage(imageString: String, options: BitmapFactory.Options): Bitmap {
+            val bytes = Base64.decode(imageString, Base64.DEFAULT)
+            return BitmapFactory.decodeByteArray(bytes, 0, bytes.size, options)
+        }
+    }
+}
